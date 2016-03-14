b851a3ef5dea61f1f0dbc80fe93de5dfc347c386
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-03-14 20:33:14 +0100

    Added barChart for to show Daily Sales

--- resources/views/reports/dailySales.blade.php
+++ resources/views/reports/dailySales.blade.php
@@ -7,10 +7,29 @@
 @section('customs_scripts')
      <!-- Data Table -->
     <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             $('#dailySales').dataTable();
         } );
+
+        var ctx = document.getElementById("barChartDailySales").getContext("2d");
+        var data = {
+            labels: ["Day1", "Day2", "Day3", "Day3", "Day4", "Day5"],
+            datasets: [{
+                label: "Daily Sales",
+                data: [950, 1589, 1589, 2500, 3400, 456],
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)"
+
+            }]
+        }
+        var myBarChart = new Chart(ctx).Bar(data);
+
     </script>
 @endsection
 @section('customs-css')
@@ -55,6 +74,27 @@
                                 </tr>
                             </thbody>
                         </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-10">
+                <div class="box box-success">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Bar Chart</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <div class="chart">
+                            <canvas id="barChartDailySales" style="height: 226px; width: 508px;" width="508" height="226"></canvas>
+                        </div>
                     </div>
                     <!-- /.box-body -->
                 </div>
