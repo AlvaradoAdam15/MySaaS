d88bbc386760f1ed3a049937587999e614cc4cdd
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-04-06 13:58:59 +0200

    Simple example to generate invoice

--- resources/views/reports/createInvoice.blade.php
+++ resources/views/reports/createInvoice.blade.php
@@ -7,8 +7,8 @@
 @section('main-content')
 
     <style>
-        .flyer-builder iframe,
-        .flyer-builder .no_iframe {
+        .invoice-builder iframe,
+        .invoice-builder .no_iframe {
             width: 100%;
             height: 400px;
             border: 1px solid #666;
@@ -20,53 +20,53 @@
     <div class="container spark-screen">
         <div class="row">
             <div class="col-xs-10">
-                <div class="container-fluid flyer-builder">
+                <div class="container-fluid invoice-builder">
                     <h1>Invoice Builder</h1>
                     <div class="row">
                         <div class="col-sm-6 loc_form">
 
                             <div class="form-group">
-                                <label for="flyer-image" class="control-label">Image</label>
-                                <input id="flyer-image" type="file" tabindex="1">
+                                <label for="invoice-image" class="control-label">Image</label>
+                                <input id="invoice-image" type="file" tabindex="1">
                             </div>
 
                             <div class="row">
                                 <div class="col-sm-7">
                                     <div class="form-group">
-                                        <label for="flyer-title" class="control-label">Title</label>
-                                        <input required class="form-control" id="flyer-title" placeholder="Main title" maxlength="255" type="text" tabindex="2">
+                                        <label for="invoice-title" class="control-label">Title</label>
+                                        <input required class="form-control" id="invoice-title" placeholder="Main title" maxlength="255" type="text" tabindex="2">
                                     </div>
                                 </div>
                                 <div class="col-sm-3">
                                     <div class="form-group">
-                                        <label for="flyer-title-size" class="control-label">Size (pt)</label>
-                                        <input required class="form-control" id="flyer-title-size" value="60" min="1" step="1" type="number" tabindex="3" title="Title size: tune it to fit the available space">
+                                        <label for="invoice-title-size" class="control-label">Size (pt)</label>
+                                        <input required class="form-control" id="invoice-title-size" value="60" min="1" step="1" type="number" tabindex="3" title="Title size: tune it to fit the available space">
                                     </div>
                                 </div>
                                 <div class="col-sm-2">
                                     <div class="form-group">
-                                        <label for="flyer-title-color" class="control-label">Color</label>
-                                        <input required class="form-control" id="flyer-title-color" value="#0080FF" type="color" tabindex="4" title="Title color">
+                                        <label for="invoice-title-color" class="control-label">Color</label>
+                                        <input required class="form-control" id="invoice-title-color" value="#0080FF" type="color" tabindex="4" title="Title color">
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label class="control-label" for="flyer-description">Description</label>
-                                <textarea class="form-control" id="flyer-description" placeholder="Insert a short description taking care of the available space"></textarea>
+                                <label class="control-label" for="invoice-description">Description</label>
+                                <textarea class="form-control" id="invoice-description" placeholder="Insert a short description taking care of the available space"></textarea>
                             </div>
 
                             <div class="row">
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <label for="flyer-price" class="control-label">Price</label>
-                                        <input required class="form-control" id="flyer-price" placeholder="Enter price (w/o decimals)" value="100" type="number" tabindex="5" step="any" min="0">
+                                        <label for="invoice-price" class="control-label">Price</label>
+                                        <input required class="form-control" id="invoice-price" placeholder="Enter price (w/o decimals)" value="100" type="number" tabindex="5" step="any" min="0">
                                     </div>
                                 </div>
                                 <div class="col-sm-3">
                                     <div class="form-group">
-                                        <label for="flyer-price-currency" class="control-label">Currency</label>
-                                        <select required class="form-control" id="flyer-price-currency" tabindex="6">
+                                        <label for="invoice-price-currency" class="control-label">Currency</label>
+                                        <select required class="form-control" id="invoice-price-currency" tabindex="6">
                                             <option value="&euro;">&euro;</option>
                                             <option value="$">$</option>
                                             <option value="&pound;">&pound;</option>
@@ -75,8 +75,8 @@
                                 </div>
                                 <div class="col-sm-2">
                                     <div class="form-group">
-                                        <label for="flyer-price-color" class="control-label">Color</label>
-                                        <input required class="form-control" id="flyer-price-color" value="#cc0000" type="color" tabindex="8" title="Price color">
+                                        <label for="invoice-price-color" class="control-label">Color</label>
+                                        <input required class="form-control" id="invoice-price-color" value="#cc0000" type="color" tabindex="8" title="Price color">
                                     </div>
                                 </div>
                             </div>
@@ -84,12 +84,12 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <button onclick="preview()" id="flyer_preview_btn" type="button" class="btn btn-primary btn-block" tabindex="9">Update preview</button>
+                                        <button onclick="preview()" id="invoice_preview_btn" type="button" class="btn btn-primary btn-block" tabindex="9">Update preview</button>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-group text-right">
-                                        <button onclick="download()" id="flyer_download_btn" type="button" class="btn btn-default btn-xs btn-block" tabindex="10">Download</button>
+                                        <button onclick="download()" id="invoice_download_btn" type="button" class="btn btn-default btn-xs btn-block" tabindex="10">Download</button>
                                     </div>
                                 </div>
                             </div>
@@ -110,23 +110,67 @@
 
     <script>
 
+        var logo;
+
         function generatePDF(){
-            var pdf = new jsPDF('p', 'mm', 'a4');
-            pdf.text(30, 30, 'Hello world!');
+            this.pdf = new jsPDF('p', 'mm', 'a4');
+            this.title = document.getElementById('invoice-title').value;
+            this.titleColor = document.getElementById('invoice-title-color').value;
+            this.titleSize = document.getElementById('invoice-title-size').value;
+            this.description = document.getElementById('invoice-description').value;
+            this.price = document.getElementById('invoice-price').value;
+            this.currency = document.getElementById('invoice-price-currency').value;
+            this.priceColor = document.getElementById('invoice-price-color').value;
+            this.separator = "-------------------------------------------------------------------------"
+            this.titleSeparation = 30;
+
+            if (logo != null){
+                pdf.addImage(logo, 'JPEG', 10, 5, 80, 60);
+                this.titleSeparation = 90;
+            }
+
+            pdf.setFontSize(titleSize);
+            pdf.setTextColor(titleColor);
+            this.titleWidth = pdf.getStringUnitWidth(title) * titleSize / pdf.internal.scaleFactor;
+            this.titleOffset = (pdf.internal.pageSize.width - titleWidth) / 2;
+
+            pdf.text(title, titleOffset, titleSeparation);
+            pdf.setFontSize(25);
+            pdf.setTextColor("black");
+            pdf.text(description , 10, titleSeparation+20);
+            this.priceWidth = pdf.getStringUnitWidth(price) * 25 / pdf.internal.scaleFactor;
+            this.priceOffset = pdf.internal.pageSize.width - priceWidth - 30;
+            pdf.text(separator, 0, pdf.internal.pageSize.width-10);
+            pdf.text("Price:", 10, pdf.internal.pageSize.width);
+            pdf.setTextColor(priceColor);
+            pdf.text(price+currency, priceOffset,pdf.internal.pageSize.width);
             return pdf;
         }
 
 
         function download() {
-            var pdf = generatePDF();
-            pdf.save(document.getElementById('flyer-title').value + '.pdf');
+            this.pdf = generatePDF();
+            this.pdf.save(document.getElementById('invoice-title').value + '.pdf');
         }
 
         function preview() {
-            var pdf = generatePDF();
-            var string = pdf.output('datauristring');
+            this.pdf = generatePDF();
+            this.string = pdf.output('datauristring');
             $('#pdf_preview').attr('src', string);
         }
+
+        function getBase64() {
+            logo = null;
+            var FR= new FileReader();
+            FR.onload = function(e) {
+                logo = e.target.result;
+
+            };
+            FR.readAsDataURL(this.files[0]);
+
+        }
+
+        document.getElementById('invoice-image').addEventListener("change", getBase64, false);
 
     </script>
 
