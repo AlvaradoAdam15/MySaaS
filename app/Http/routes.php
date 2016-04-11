80ffd02278044cedd16b21b35b47270515461c46
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-04-11 19:28:05 +0200

    Fixed errors for pull request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -10,10 +9,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -26,16 +21,42 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    Route::get('auth/github', 'Auth\AuthController@redirectToGithubProvider');
-    Route::get('auth/github/callback', 'Auth\AuthController@handleGithubProviderCallback');
-    Route::get('auth/facebook', 'Auth\AuthController@redirectToFacebookProvider');
-    Route::get('auth/facebook/callback', 'Auth\AuthController@handleFacebookProviderCallback');
-    Route::get('auth/google', 'Auth\AuthController@redirectToGoogleProvider');
-    Route::get('auth/google/callback', 'Auth\AuthController@handleGoogleProviderCallback');
+App::bind('Flash',
+    App\Http\Flash::class);
 
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/',['as' => 'welcome', function () {
+        return view('welcome');
+    }]);
+
+    Route::get('auth/{provider}', 'Auth\SocialAuthController@redirectToAuthenticationServiceProvider');
+    Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleAuthenticationServiceProviderCallback');
     Route::get('csstransitions', function(){
         return view('tinkering.csstransitions');
+    });
+    Route::get('plans', 'PlansController@index');
+    Route::get('register_subsciption', function(){
+        return view('auth.register_subsciption');
+    });
+
+    Route::post('sendContactEmail','ContactEmailController@send');
+
+    Route::post('registerAndSubscribeToStripe', 'Auth\AuthController@registerAndSubscribeToStripe');
+
+    Route::get('reports/dailySales', 'ReportsController@dailySales');
+
+    Route::get('reports/downloadInvoice', 'PDFController@invoiceHtml');
+
+    Route::get('reports/createInvoice', 'CreateInvoiceController@index');
+
+    Route::get('users', 'UsersController@index');
+    Route::post('users', 'UsersController@store');
+    Route::delete('users', 'UsersController@delete');
+    Route::put('users', 'UsersController@update');
+
+    Event::listen('user.change', function(){
+        Cache::forget('query.users');
     });
 
     Route::get('sendpushnotify', function(){
