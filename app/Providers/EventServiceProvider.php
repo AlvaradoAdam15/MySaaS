cee135da0f63796d856ea88bdbc3ae048ff03737
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-04-18 16:11:05 +0200

    Created Event for UserLoged and Listener for UserUpdateLastLogin

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\SomeEvent' => [
             'App\Listeners\EventListener',
         ],
+        'App\Events\UserLoged' => [
+            'App\Listeners\UserUpdateLastLogin',
+        ],
     ];
 
     /**
